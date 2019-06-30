649f66c34d82c087948fa3533c6af52f5eaa3cac
hjohnson12 <hunter.johnson512@gmail.com>
2019-06-30 11:40:32 -0400

    Model properties added to VM

--- KanbanBoardUWP/ViewModel/MainViewModel.cs
+++ KanbanBoardUWP/ViewModel/MainViewModel.cs
@@ -13,10 +13,63 @@ namespace KanbanBoardUWP.ViewModel
     {
         public ObservableCollection<KanbanModel> Tasks { get; }
 
+        private KanbanModel Model = new KanbanModel();
 
         public MainViewModel()
         {
             Tasks = DataAccess.GetData();
         }
+
+        public string ID
+        {
+            get { return Model.ID; }
+            set
+            {
+                Model.ID = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Title
+        {
+            get { return Model.Title; }
+            set
+            {
+                Model.Title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Description
+        {
+            get { return Model.Description; }
+            set
+            {
+                Model.Description = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public object Category
+        {
+            get { return Model.Category; }
+            set
+            {
+                Model.Category = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public object ColorKey
+        {
+            get { return Model.ColorKey; }
+            set
+            {
+                Model.ColorKey = value;
+                OnPropertyChanged();
+            }
+        }
+
+
     }
 }
