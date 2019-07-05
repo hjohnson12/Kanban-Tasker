6ca377b8fc346f176c44652e3dcec8e53028ecf0
hjohnson12 <hunter.johnson512@gmail.com>
2019-07-04 21:14:19 -0400

    UserControl view moved to appropriate folder

--- KanbanBoardUWP/Views/BoardUserControl.xaml.cs
+++ KanbanBoardUWP/Views/BoardUserControl.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Navigation;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
-namespace KanbanBoardUWP
+namespace KanbanBoardUWP.Views
 {
     public sealed partial class BoardUserControl : UserControl
     {
