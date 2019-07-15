ef2885e75c62bdb260c40cd3316990dd162f7a76
hjohnson12 <hunter.johnson512@gmail.com>
2019-07-14 20:42:14 -0400

    MainViewModel updated

--- KanbanTasker/ViewModels/MainViewModel.cs
+++ KanbanTasker/ViewModels/MainViewModel.cs
@@ -1,16 +1,59 @@
-﻿using System;
+﻿using KanbanTasker.Base;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KanbanTasker.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : Observable
     {
+        private ObservableCollection<BoardViewModel> _boardList;
+        private BoardViewModel _current;
+
         public MainViewModel()
         {
+            //Your constructor
+
+            //Instanciate the list object.  You bind your XAML list to this in 'ItemSource'
+            BoardList = new ObservableCollection<BoardViewModel>();
+
+            //Create your Board.
+            BoardViewModel myBoard = new BoardViewModel();
+            //Load your board (i dont know how you do this in your setup yet)
+
+            //Add it to the list
+            BoardList.Add(myBoard);
+        }
+
+        public ObservableCollection<BoardViewModel> BoardList
+        {
+            get
+            {
+                return _boardList;
+            }
+            set
+            {
+                BoardList = value;
+                OnPropertyChanged("BoardList");
+            }
+        }
+
+        public BoardViewModel Current
+        {
+            get
+            {
+                return _current;
+            }
+            set
+            {
+                _current = value;
+                OnPropertyChanged("Current"); // Let the UI know to update this binding.
+            }
 
         }
+
     }
 }
