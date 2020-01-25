fc37c073e49eecca04fa1b4c18e6e76c47288cf4
hjohnson12 <hunter.johnson512@gmail.com>
2020-01-25 01:05:06 -0500

    StringToDateConverter added

---
+++ KanbanTasker/Converters/Text/StringToDateConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace KanbanTasker.Converters.Text
+{
+    public class StringToDateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            DateTimeOffset dt;
+            if (value != null && value is string)
+            {
+                var stringToConvert = value as string;
+                if (!DateTimeOffset.TryParse(stringToConvert, out dt))
+                    return null;
+            }
+            return dt;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            string time;
+            if (value != null && value is DateTimeOffset)
+            {
+                var valueToConvert = (DateTimeOffset)value;
+                time = new DateTime(valueToConvert.Ticks).ToString("yyyy.MM.dd");
+            }
+            else
+                time = string.Empty;
+            return time;
+        }
+    }
+}
