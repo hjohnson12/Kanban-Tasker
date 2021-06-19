263835af00a31460e8b7262536b0e3796fe3d379
hjohnson12 <hunter.johnson512@gmail.com>
2021-06-19 17:53:52 -0400

    Updated relay comand class

--- KanbanTasker/Base/RelayCommand.cs
+++ KanbanTasker/Base/RelayCommand.cs
@@ -7,7 +7,7 @@ using System.Windows.Input;
 
 namespace KanbanTasker.Base
 {
-
+   
     public class RelayCommand<T> : ICommand
     {
         public event EventHandler CanExecuteChanged;
@@ -21,6 +21,7 @@ namespace KanbanTasker.Base
 
         public RelayCommand(Action<T> action, Func<bool> canExecute)
         {
+
             this.action = action;
             this._canExecute = canExecute;
         }
@@ -35,18 +36,33 @@ namespace KanbanTasker.Base
             T t = (T)parameter;
             action(t);
         }
+
+        public void RaiseCanExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
     }
 
-    
+    /// <summary>
+    /// A command whose sole purpose is to relay its functionality 
+    /// to other objects by invoking delegates. 
+    /// The default return value for the CanExecute method is 'true'.
+    /// <see cref="RaiseCanExecuteChanged"/> needs to be called whenever
+    /// <see cref="CanExecute"/> is expected to return a different value.
+    /// </summary>
     public class RelayCommand : ICommand
     {
-        public event EventHandler CanExecuteChanged;
         private readonly Action _execute;
         private readonly Func<bool> _canExecute;
         /// <summary>
         /// Raised when RaiseCanExecuteChanged is called.
         /// </summary>
-        
+        public event EventHandler CanExecuteChanged;
+
         /// <summary>
         /// Creates a new command that can always execute.
         /// </summary>
@@ -67,6 +83,7 @@ namespace KanbanTasker.Base
             _execute = execute;
             _canExecute = canExecute;
         }
+
         /// <summary>
         /// Determines whether this RelayCommand can execute in its current state.
         /// </summary>
@@ -79,6 +96,7 @@ namespace KanbanTasker.Base
         {
             return _canExecute == null ? true : _canExecute();
         }
+
         /// <summary>
         /// Executes the RelayCommand on the current command target.
         /// </summary>
@@ -90,6 +108,7 @@ namespace KanbanTasker.Base
         {
             _execute();
         }
+
         /// <summary>
         /// Method used to raise the CanExecuteChanged event
         /// to indicate that the return value of the CanExecute
