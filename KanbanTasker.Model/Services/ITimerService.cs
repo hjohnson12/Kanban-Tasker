91d60e1c2eda3e7a5d5a86f9b9d958cde13291a2
hjohnson12 <hunter.johnson512@gmail.com>
2022-01-29 02:42:40 -0500

    Basic TimerService class added to replace implementation in CalendarViewModel to remove UI reference from VM

---
+++ KanbanTasker.Model/Services/ITimerService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KanbanTasker.Model.Services
+{
+    public interface ITimerService
+    {
+        void ConfigureTimer();
+        void StartTimer();
+        void StopTimer();
+    }
+}
---
+++ KanbanTasker/Services/TimerService.cs
@@ -0,0 +1,39 @@
+﻿using KanbanTasker.Model.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace KanbanTasker.Services
+{
+    public class TimerService : ITimerService
+    {
+        private DispatcherTimer _timer;
+
+        public DateTime Time { get; set; }
+
+        public void ConfigureTimer()
+        {
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += Timer_Tick; ;
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            Time = DateTime.Now;
+        }
+
+        public void StartTimer()
+        {
+            _timer.Start();
+        }
+
+        public void StopTimer()
+        {
+            _timer.Stop();
+        }
+    }
+}
